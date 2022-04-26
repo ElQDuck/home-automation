0b5ce98e31fdb6241e63f37443c4565f3ea61b9b
QDuck <qduck@mail.ru>
2022-04-26 18:01:10 +0200

    feat: optimizing code

--- sensors/temperature-humidity/BME280/battery-powered/platformio/arduino-pro-mini-8MHzatmega328/src/main.cpp
+++ sensors/temperature-humidity/BME280/battery-powered/platformio/arduino-pro-mini-8MHzatmega328/src/main.cpp
@@ -10,15 +10,18 @@
 #include "LowPower.h"
 
 // Function Declarations
+struct BME_VALUES {float humidity; float pressure; float altitude; float temperature;};
+
 String GetArduinoUniqueID();
-long readVcc();
-void log(String logMessage);
-void readBmeValues();
+long ReadVcc();
+void Log(String logMessage);
+BME_VALUES ReadBmeValues();
+
 
 // Constants
 bool DEBUG = true; // Set true to activate serial messages, false in final code.
 const String ARDUINO_ID = GetArduinoUniqueID(); // The unique Arduino ID
-const uint16_t other_node = 00;      // Address of the other node (gateway) in Octal format
+const uint16_t other_node = 00;      // Address of the other node (gateway) in Octal format TODO: Rename variable name
 const uint16_t this_node = 01;       // Address of our node (sensor) in Octal format
 
 struct payload_t {                   // Structure of our payload
@@ -35,7 +38,7 @@ RF24Network network(radio);          // Network uses that radio
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // BME280
   Wire.begin();
@@ -45,7 +48,7 @@ void setup()
   bme.setMode(MODE_SLEEP); //Sleep for now
 
   if (!radio.begin()) {
-    log("Radio hardware not responding!");
+    Log("Radio hardware not responding!");
     while (1) {
       // hold in infinite loop
     }
@@ -56,63 +59,65 @@ void setup()
 
 void loop()
 {
-  readBmeValues();
+  sleepCounter ++;  // The counter for the sleep cycles
 
-  // sleepCounter ++;  // The counter for the sleep cycles
+  // The deep sleep mode can only sleep for max. 8 seconds.
+  // For a sleep period of >8s we have to go in sleep mode periodicaly.
+  // Example: Sleep for 16s: sleepCounter >= 2 -> 2*8s=16s
+  // If in debug mode sleep for 16 seconds, if not sleep for 4 minutes
+  if (sleepCounter >= (DEBUG ? 2 : 30))
+  {
+    BME_VALUES sensorValues = ReadBmeValues();
 
-  // // The deep sleep mode can only sleep for max. 8 seconds.
-  // // For a sleep period of >8s we have to go in sleep mode periodicaly.
-  // // Example: Sleep for 16s: sleepCounter >= 2 -> 2*8s=16s
-  // // If in debug mode sleep for 16 seconds, if not sleep for 4 minutes
-  // if (sleepCounter >= (DEBUG ? 2 : 30))
-  // {
-  //   //TODO: use sleep mode for nrf24 https://nrf24.github.io/RF24/examples_2old_backups_2pingpair_sleepy_2pingpair_sleepy_8ino-example.html
+    // JSON Example:
+    // {
+    // 	  "DeviceID": "88255223255823855255255255",
+    //    "BatteryVoltage" 3.15, (V)
+    //    "SensorType": "BME280",
+    // 	  "Temperature": 21.1 (°C)
+    // 	  "Humidity": 48.0, (%)
+    //    "Pressure": 996.63 (hPa)
+    // }
+    
 
-  //   float humidity = 59.58; //dht.getHumidity();
-  //   float temperature = 22.34; //dht.getTemperature();
-  //   String json = "";
+    // TODO: replace json with char[]
 
-  //   // Print value if both values are real numbers (prevent NaN print)
-  //   if (!isnan(humidity) && !isnan(temperature))
-  //   {
-  //     // JSON Example:
-  //     // {
-  //     // 	  "DeviceID": "88255223255823855255255255",
-  //     //    "BatteryVoltage" 3.15, (V)
-  //     //    "SensorType": "BME280",
-  //     // 	  "Humidity": 48.0, (%)
-  //     // 	  "Temperature": 21.1 (°C)
-  //     // }
-  //     json = "{\"DeviceID\":\"" + String(ARDUINO_ID) + "\", \
-  //             \"BatteryVoltage\":" + readVcc() / 1000. + ", \
-  //             \"SensorType\":\"BME280\", \
-  //             \"Humidity\":" + String(humidity) + ", \
-  //             \"Temperature\":" + String(temperature) + "}";
-  //     log(json);
-  //   }
+    String json = "askldjalskdjaskldjalskdjaskldjalskdjaskldjalskdjask";
+    //String json = "{\"DeviceID\":\"" + ARDUINO_ID + "\", \
+    //         \"BatteryVoltageValue\":" + ReadVcc() / 1000. + ", \
+    //         \"BatteryVoltageUnit\": \"V\", \
+    //         \"SensorType\":\"BME280\", \
+    //         \"TemperatureValue\":" + sensorValues.temperature + ", \
+    //         \"TemperatureUnit\": \"°C\", \
+    //         \"HumidityValue\":" + sensorValues.humidity + ", \
+    //         \"HumidityUnit\": \"%\", \
+    //         \"PressureValue\":" + sensorValues.pressure + ", \
+    //         \"PressureUnit\": \"hPa\"}";
+    Log(json);
 
-  //   network.update(); // Check the network regularly
-  //   // If it's time to send a message, send it!
-  //     int stringLength = json.length();
-  //     char msg[stringLength + 1];
-  //     strcpy(msg, json.c_str());
+    radio.powerUp();  // Wake up the RF24 from sleep mode
+    network.update(); // Check the network regularly
 
-  //     log("Sending... ");
-  //     log(json);
-  //     log("With size:");
-  //     uint16_t msgLength = sizeof(msg);
-  //     log(String(msgLength));
-  //     RF24NetworkHeader header(/*to node*/ other_node);
-  //     bool ok = network.write(header, &msg, msgLength);
-  //     log(ok ? "ok." : "failed.");
+    // Convert the json string into a char array because we cant use the String for sending
+    int stringLength = json.length();
+    char msg[stringLength + 1];
+    strcpy(msg, json.c_str());
+    uint16_t msgLength = sizeof(msg);
 
-  //   sleepCounter = 0; // Reset the sleep counter
-  // }
-  // // Sleep for 8 seconds
-  // log("Going to sleep.");
-  // delay(100);
-  // LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); // For more information on the different types of sleep mode refer to: https://elektro.turanis.de/html/prj270/index.html
-  // log("im awake");
+    Log("Sending... " + json);
+    Log("With size: " + String(msgLength));
+    RF24NetworkHeader header(other_node); // To Gateway
+    bool ok = network.write(header, &msg, msgLength);
+    Log(ok ? "ok." : "failed.");
+
+    radio.powerDown();  // Put the RF24 into sleep mode: https://nrf24.github.io/RF24/examples_2old_backups_2pingpair_sleepy_2pingpair_sleepy_8ino-example.html
+    sleepCounter = 0;   // Reset the sleep counter to put the Arduino into sleep mode
+    delay(100);         // delay before go to sleep to finish background work
+  }
+
+  // Sleep
+  // For more information on the different types of sleep mode refer to: https://elektro.turanis.de/html/prj270/index.html
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
 }
 
 // Function to get the ArduinoUniqueID
@@ -127,10 +132,10 @@ String GetArduinoUniqueID(){
   return id;
 }
 
+// Reads the Voltage in mV
 // TODO: make a reference measurement
 // Used from https://forum.arduino.cc/t/how-to-know-vcc-voltage-in-arduino/344001
-// Reads the Voltage in mV
-long readVcc() {
+long ReadVcc() {
   long result;
   // Read 1.1V reference against AVcc
   ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
@@ -144,7 +149,7 @@ long readVcc() {
 }
 
 // A logger for debugging
-void log(String logMessage){
+void Log(String logMessage){
   if(DEBUG){
     Serial.println(logMessage);
   }
@@ -153,25 +158,23 @@ void log(String logMessage){
 // Read values from BME280 Sensor
 // Code from: https://github.com/sparkfun/SparkFun_BME280_Arduino_Library/blob/master/examples/Example6_LowPower/Example6_LowPower.ino
 // TODO: Return Values (replace void)
-void readBmeValues() {
-  bme.setMode(MODE_FORCED); //Wake up sensor and take reading
-  while(bme.isMeasuring() == false) ; //Wait for sensor to start measurment
-  while(bme.isMeasuring() == true) ;  //Hang out while sensor completes the reading
+BME_VALUES ReadBmeValues() {
+  BME_VALUES returnValue;
+
+  bme.setMode(MODE_FORCED);           // Wake up sensor and take reading
+  while(bme.isMeasuring() == false);  // Wait for sensor to start measurment
+  while(bme.isMeasuring() == true);   // Hang out while sensor completes the reading
 
   // Sensor is now back asleep but the data can be read from its registers
-  Serial.print(" Humidity: ");
-  Serial.print(bme.readFloatHumidity(), 0);
-  Serial.println();
-  Serial.print(" Pressure: "); // 96386.2 Pa = 963.862 hPa
-  Serial.print(String(bme.readFloatPressure() / 100));
-  Serial.println();
-  Serial.print(" Alt: ");
-  Serial.print(bme.readFloatAltitudeMeters(), 1);
-  //Serial.print(bme.readFloatAltitudeFeet(), 1);
-  Serial.println();
-  Serial.print(" Temp: ");
-  Serial.print(bme.readTempC(), 2);
-  //Serial.print(bme.readTempF(), 2);
-  Serial.println();
-  delay(1000);
+  returnValue.humidity = bme.readFloatHumidity();       // in %
+  returnValue.pressure = bme.readFloatPressure() / 100; // in hPa
+  returnValue.altitude = bme.readFloatAltitudeMeters(); // in m
+  returnValue.temperature = bme.readTempC();            // in °C
+
+  Log("Humidity: " + String(returnValue.humidity) + "%");
+  Log("Pressure: " + String(returnValue.pressure) + "hPa");
+  Log("Altitude: " + String(returnValue.altitude) + "m");
+  Log("Temperature: " + String(returnValue.temperature) + "°C");
+
+  return returnValue;
 }
